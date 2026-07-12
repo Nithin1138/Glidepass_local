b853e07b63f67f83a4d4041be1f43aac3ac0f137
Nithin1138 <veeranithin9@gmail.com>
2026-07-12 16:23:10 +0530

    chore: remove test dart files again

--- lanpad_mobile/test_uri_auth.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  final uri1 = Uri.parse('https://abc.trycloudflare.com?sid=123');
-  print('1: \${uri1.scheme}://\${uri1.authority}');
-  
-  final uri2 = Uri.parse('http://192.168.0.106:8000?sid=123');
-  print('2: \${uri2.scheme}://\${uri2.authority}');
-}
